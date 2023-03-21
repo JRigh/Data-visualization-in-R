627026ef17d8be8c67df1f211efeadfd2c4894d6
Julian <julian.righ.sampedro@gmail.com>
2023-03-21 15:45:33 +0100

    Add files via upload

---
+++ parallel coordinates plot.R
@@ -0,0 +1,20 @@
+#---------------------------
+# Parallel coordinates plots
+#---------------------------
+
+library(GGally)
+
+ggparcoord(iris, columns = 1:4, groupColumn = 5, order = "anyClass",
+           showPoints = TRUE, alphaLines = 0.4) + 
+  labs(title = 'Parallel coordinates plot',
+       subtitle = 'iris dataset',
+       y="value", x="Species") +
+  theme(axis.text=element_text(size=8),
+        axis.title=element_text(size=8),
+        plot.subtitle=element_text(size=10, face="italic", color="darkred"),
+        panel.background = element_rect(fill = "white", colour = "grey50"),
+        panel.grid.major = element_line(colour = "grey90"))
+
+#----
+# end
+#----
